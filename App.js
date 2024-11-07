1c2de0350917dcbf20ff1e2778ff2234340a0c12
Sobhit K <sobhitk@ecomexpress.in>
2024-11-08 05:18:31 +0530

    Exploring React as Lib

---
+++ App.js
@@ -0,0 +1,35 @@
+/***
+ * html structure
+ *   <div id="parent">
+        <div id="child">
+            <h1> I am H1 tag</h1>
+            <h2>I am h2 tag</h2>
+        </div>
+        <div id="child">
+            <h1> I am H1 tag</h1>
+            <h2>I am h2 tag</h2>
+        </div>
+    </div>
+ */
+
+
+const heading = React.createElement("div", {
+            id:"parent",
+        }, [
+            React.createElement("div", {
+            id: "child"}
+            , [
+                React.createElement("h1", {}, "I am H1 tag."), 
+                React.createElement("h2", {}, "I am H2 tag.")]),
+
+            React.createElement("div", {
+            id: "child"}
+            , [
+                React.createElement("h1", {}, "I am H1 tag."), 
+                React.createElement("h2", {}, "I am H2 tag.")])
+        
+        ]);
+
+console.log(heading) // object
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
