187a7b47b5e11f607be94d2077993f4c94ecb433
Sobhit K <sobhit2010@gmail.com>
2024-11-20 22:20:38 +0530

    using parcel to build

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 /***
  * html structure
  *   <div id="parent">
@@ -14,21 +17,23 @@
 
 
 const heading = React.createElement("div", {
-            id:"parent",
-        }, [
-            React.createElement("div", {
-            id: "child"}
-            , [
-                React.createElement("h1", {}, "I am H1 tag."), 
-                React.createElement("h2", {}, "I am H2 tag.")]),
+    id: "parent",
+}, [
+    React.createElement("div", {
+        id: "child"
+    }
+        , [
+            React.createElement("h1", {}, "I am H1 tag. test 2"),
+            React.createElement("h2", {}, "I am H2 tag. 2")]),
 
-            React.createElement("div", {
-            id: "child"}
-            , [
-                React.createElement("h1", {}, "I am H1 tag."), 
-                React.createElement("h2", {}, "I am H2 tag.")])
-        
-        ]);
+    React.createElement("div", {
+        id: "child"
+    }
+        , [
+            React.createElement("h1", {}, "I am H1 tag."),
+            React.createElement("h2", {}, "I am H2 tag.")])
+
+]);
 
 console.log(heading) // object
 const root = ReactDOM.createRoot(document.getElementById('root'));
