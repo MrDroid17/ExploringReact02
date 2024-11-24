102e91a8b3bb5dd89502fbb72ed961bab168ec9a
Sobhit K <sobhit2010@gmail.com>
2024-11-24 18:46:05 +0530

    basic of parcel, babel, functional sn components

--- App.js
+++ App.js
@@ -1,40 +1,42 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-/***
- * html structure
- *   <div id="parent">
-        <div id="child">
-            <h1> I am H1 tag</h1>
-            <h2>I am h2 tag</h2>
-        </div>
-        <div id="child">
-            <h1> I am H1 tag</h1>
-            <h2>I am h2 tag</h2>
-        </div>
-    </div>
+// const heading = React.createElement('h1', { id: "heading" }, "Namesty React");
+// console.log(heading);
+
+// const jsxHeading = <h1 id="heading" className="blue">Namesty React Using JSX</h1>
+// console.log(jsxHeading);
+
+
+/**
+ * 2 ways to create component
+ * 1. class based component
+ * 2. functional component -> normal js function which is returing jsx or reactelement or multiple reactElelment
+ *                           Also its name should start with Capital letter with camel case 
+ * 
  */
+// functional component
+const ele = <span>React Element span | </span>
+const Title = () => (
+    <h1 className="head" tabIndex='1'>
+        {ele}
+        Namesty React title
+    </h1>
+)
+
+const HeadingComponent = () => (
+    <div id="container">
+        <h2>{1000}</h2>
+        <h2>{console.log("testing....")}</h2>
+        <Title />
+        <h1 id="heading" className="blue">Namesty React Functional component</h1>
+    </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
-const heading = React.createElement("div", {
-    id: "parent",
-}, [
-    React.createElement("div", {
-        id: "child"
-    }
-        , [
-            React.createElement("h1", {}, "I am H1 tag. test 2"),
-            React.createElement("h2", {}, "I am H2 tag. 2")]),
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);
 
-    React.createElement("div", {
-        id: "child"
-    }
-        , [
-            React.createElement("h1", {}, "I am H1 tag."),
-            React.createElement("h2", {}, "I am H2 tag.")])
-
-]);
-
-console.log(heading) // object
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
