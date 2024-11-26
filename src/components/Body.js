c38fda6b5df31ae952c44878df53c8b4d2ee29fe
Sobhit K <sobhit2010@gmail.com>
2024-11-26 22:38:01 +0530

    Using useState Hook to maintain state

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,32 @@
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 import resList from "../utils/mockData";
 
+
+
 const Body = () => {
+
+    const [listOfRestaurant, setListOfRestaurant] = useState(resList)
+    const [restaurantCount, setRestaurantCount] = useState(resList.length)
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    const filterList = listOfRestaurant.filter(res => res.info.avgRating > 4.5)
+                    setListOfRestaurant(filterList);
+                    setRestaurantCount(filterList.length);
+                }}
+                >Top Rated Restaurant</button>
+                <button className="filter-btn" onClick={() => {
+                    setListOfRestaurant(resList);
+                    setRestaurantCount(resList.length);
+                }}
+                >Clear Filter</button>
+                <div>Count {restaurantCount}</div>
+            </div>
             <div className="restaurant-container">
                 {
-                    resList.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant} />)
+                    listOfRestaurant.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant} />)
                 }
 
             </div>
